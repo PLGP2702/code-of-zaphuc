c688003eee6e35625d25753be8df37f77841fe5e
PLGP2702 <phucphan2702@gmail.com>
2022-09-08 10:20:26 +0700

    plgp

--- pythoncode/codelearn/chuong2/bai10.py
+++ pythoncode/codelearn/chuong2/bai10.py
@@ -2,3 +2,9 @@ name = 'Phuc'
 ngaysinh = 2702
 print("Ten :" + name)
 print("ngaysinh :"+ str(ngaysinh))
+
+#----------------------------------
+name = 'Gia Dat'
+birthday = '0910'
+print("Ten : " + name)
+print("birthday : " + str(birthday))
