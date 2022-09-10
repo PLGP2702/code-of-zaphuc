08f3fcd13c9b340deb69ce825907b2d33382816f
PLGP2702 <phucphan2702@gmail.com>
2022-09-10 16:30:21 +0700

    123

---
+++ PLGP2702/python/kteam/chuoi/string.py
@@ -0,0 +1,17 @@
+#nháy đơn
+''
+
+#nháy kép
+""
+
+''' '''
+""" """
+#-----------------
+kzaphuc = '''
+Za Phuc, I'm Beginner
+HowKteam.com
+Web Programming
+'''
+print(kzaphuc)
+print(type(kzaphuc))
+#SOLID
