1dc16681913c7c181c21dfa46f93d2ffe828db23
PLGP2702 <phucphan2702@gmail.com>
2022-09-11 12:08:46 +0700

    123

---
+++ PLGP2702/python/selflearning/nhapdulieu.py
@@ -0,0 +1,4 @@
+#dùng int, float, ... định dạng dữ liệu trước input
+n = int(input("Nhap so nguyen n: "))
+Diem_TB = float(input("Cho biet diem trung binh: "))
+print("Diem trung binh vua nhap: ", Diem_TB)
