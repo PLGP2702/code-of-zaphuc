bc5b2a8731c4e6bd6d341555890888f1a41b71e6
PLGP2702 <phucphan2702@gmail.com>
2022-09-11 20:27:40 +0700

    gia phuc

--- PLGP2702/python/selflearning/caclenhrenhanh/diemthi.py
+++ PLGP2702/python/selflearning/caclenhrenhanh/diemthi.py
@@ -5,4 +5,11 @@ d_h = float(input("Nhap diem hoa: "))
 tong_diem = d_t + d_l + d_h
 loai = ""
 
-if(tong_diem> = 0)
+if(tong_diem >= 0) and (tong_diem <= 2):loai = "kem"
+if(tong_diem >= 3) and (tong_diem < 5):loai = "yeu"
+if(tong_diem >= 5) and (tong_diem <= 6):loai = "tb"
+if(tong_diem >= 71) and (tong_diem <= 8):loai = "kha"
+if tong_diem > 8: loai ="gioi"
+
+print("Tong diem 3 mon thi = ", tong_diem)
+print("Thi sinh na2y xep loai:", loai)
