8e5a58bd6bb280494047103073a50626190a4be2
PLGP2702 <phucphan2702@gmail.com>
2022-09-14 20:56:06 +0700

    23412341234

---
+++ PLGP2702/python/selflearning/caclenhrenhanh/xeploaihocluc.py
@@ -0,0 +1,19 @@
+tb = float(input("nhap diem trung binh: "))
+if tb <= 3.4:
+    print("kem.")
+elif tb < 5:
+    print("yeu.")
+elif tb <= 6.4:
+    print("trung binh")
+elif tb < 7.9:
+    print("kha.")
+else:
+    print("gioi.")
+
+
+
+
+
+
+
+
