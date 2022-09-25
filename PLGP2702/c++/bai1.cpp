2246c4980f08b869d65515c3ef828da8cf2f8705
PLGP2702 <phucphan2702@gmail.com>
2022-09-25 21:10:37 +0700

    giaphuc

--- PLGP2702/c++/bai1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
- 
-using namespace std;
-int main(){
-    cout << sizeof(bool) << endl;
-    cout << INT_MIN << " " << INT_MAX << endl;
-    cout << LLONG_MIN << " " << LLONG_MAX << endl;
-    return 0;
-}
