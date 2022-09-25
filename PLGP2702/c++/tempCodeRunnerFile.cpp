39a37b51d20257b4eed15c48c592419d72f56170
PLGP2702 <phucphan2702@gmail.com>
2022-09-25 11:12:18 +0700

    12341

---
+++ PLGP2702/c++/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+using namespace std;
+int main(){
+    cout << sizeof(bool) << endl;
+    cout << INT_MIN << " " << INT_MAX << endl;
+    cout << LLONG_MIN << " " << LLONG_MAX << endl;
+    return 0;
+}
