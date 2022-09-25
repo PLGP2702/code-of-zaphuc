1b70eb5ee08df547cebc33d25027552c6efc4375
PLGP2702 <phucphan2702@gmail.com>
2022-09-25 11:12:13 +0700

    123

--- PLGP2702/c++/helloworld.cpp
+++ PLGP2702/c++/helloworld.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    cout << "Hello world!" << endl;
+// int main() {
+//     cout << "Hello world!" << endl;
+//     return 0;
+// }
+using namespace std;
+int main(){
+    cout << sizeof(bool) << endl;
+    cout << INT_MIN << " " << INT_MAX << endl;
+    cout << LLONG_MIN << " " << LLONG_MAX << endl;
     return 0;
 }
+
+//so nguyen: int, long long
+
+//so thuc: float, double
+
+//kí tự: char - 1byte: 256 kí tự 0 - 256
+
+//bool: true, false
