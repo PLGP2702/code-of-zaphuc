36b82ac57b52435aa59a0baace229d3428be6213
PLGP2702 <phucphan2702@gmail.com>
2022-09-25 10:12:06 +0700

    123

--- PLGP2702/python/N16/chuong3/b23.py
+++ PLGP2702/python/N16/chuong3/b23.py
@@ -2,7 +2,7 @@ while True:
     try:
         n = int(input("Nhap so tu nhien : "))
         s = 0
-        for i in range(1, n-1):
+        for i in range(1, n-1):             
             if n % i == 0:
                 s += 1
         if s == n:
