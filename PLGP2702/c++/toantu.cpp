9f425f1da02352ee70bd70ff1734c053cf827a8a
PLGP2702 <phucphan2702@gmail.com>
2022-09-26 16:03:04 +0700

    2378482346

--- PLGP2702/c++/toantu.cpp
+++ PLGP2702/c++/toantu.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    /* các kiểu dữ liệu
+    /* các kiểu dữ liệu-----------------------
     int a = 100; 
     long long b = 200;
     float c = 2.50;
@@ -11,12 +11,28 @@ int main(){
     char kitu = 'r';
     bool ok = true;
     */
-    /*
+    /* nhập xuất dữ liệu -----------------------
     int a; 
     cout << "xinchao !\n";
     cin >> a;
     cout << a << endl;
     */
-   
+   //toán tử gán --------------------- assigment operator
+    /*
+    int a, b, c;
+	cin >> a;
+	b = a; 
+	cout  << b << endl;
+    c = b;
+    cout << c << endl;
+    */
+    //--------------------------------- phep' toan
+    int a = 500, b = 200; 
+    int tong = a + b;
+    int hieu = a - b;
+    int tich = a * b;
+    int thuong = a / b;
+    int du = a % b;
+    cout << tong << " " << hieu << " " << tich << " " << thuong << " " << du << endl;
     return 0;
 }
