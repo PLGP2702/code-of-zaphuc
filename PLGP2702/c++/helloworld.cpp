38f8944ce77ef493c94b407e5a65dcaae616079d
PLGP2702 <phucphan2702@gmail.com>
2022-09-26 13:39:51 +0700

    123

--- PLGP2702/c++/helloworld.cpp
+++ PLGP2702/c++/helloworld.cpp
@@ -7,17 +7,26 @@
 //     return 0;
 // }
 using namespace std;
-int main(){
+int main(){ 
     cout << sizeof(bool) << endl;
     cout << INT_MIN << " " << INT_MAX << endl;
     cout << LLONG_MIN << " " << LLONG_MAX << endl;
     return 0;
 }
 
-//so nguyen: int, long long
+//kieudulieu:
+//-so nguyen: int, long long
 
-//so thuc: float, double
+//-so thuc: float, double
 
-//kí tự: char - 1byte: 256 kí tự 0 - 256
+//-kí tự: char - 1byte: 256 kí tự 0 - 256
 
-//bool: true, false
+//-bool: true, false
+
+/*bien:
+Kieu_du_lieu ten_bien;
+int bankink; float Bankinh; double chuvi;
+- không dược bắt đầu bằng chữ số 
+- không được có dấu cách 
+- không được trung với các keyword
+*/
