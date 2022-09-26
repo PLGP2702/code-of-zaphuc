0793ec9de1a144b949ac055c0892050fe7dd460c
PLGP2702 <phucphan2702@gmail.com>
2022-09-26 15:46:09 +0700

    2341234

---
+++ PLGP2702/c++/toantu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 100; 
+    long long b = 200;
+    float c = 2.50;
+    double d = 3.873483;
+    char kitu = 'r';
+    bool ok = true;
+    return 0;
+}
