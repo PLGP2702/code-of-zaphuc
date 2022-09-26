7c6707df126653adfae6d5dc5a0402a8fca0747b
PLGP2702 <phucphan2702@gmail.com>
2022-09-26 15:49:43 +0700

    12341241234

--- PLGP2702/c++/toantu.cpp
+++ PLGP2702/c++/toantu.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 int main(){
+    /* các kiểu dữ liệu
     int a = 100; 
     long long b = 200;
     float c = 2.50;
     double d = 3.873483;
     char kitu = 'r';
     bool ok = true;
+    */
+    /*
+    int a; 
+    cout << "xinchao !\n";
+    cin >> a;
+    cout << a << endl;
+    */
+   
     return 0;
 }
