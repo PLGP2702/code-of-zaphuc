3180ad00074b86b6217b0e978d36b07d8bd6d195
PLGP2702 <phucphan2702@gmail.com>
2022-09-28 17:50:25 +0700

    0909099090

---
+++ PLGP2702/c++/cautrucrenhanh.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//abs(x) : tri tuyet doi 
+//pow(a,b) : tinh a ^ b : tra ve so double
+//sqrt(n) : tin can bac hai cua n : tra ve so double 
+// round(a) : lam tron so
+//100 > 50 : tue =>
+int main(){
+    /*
+    int n = 200;
+    if((n >= 100) && (n <= 500)){
+        cout << "so chan! \n";
+    }
+    */
+    
+    return 0;  
+}
+
