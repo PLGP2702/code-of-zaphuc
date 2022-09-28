e76e54d384b785c60c26fb11adb7a2eccd0de13a
PLGP2702 <phucphan2702@gmail.com>
2022-09-28 10:51:03 +0700

    55555555555555555

--- PLGP2702/c++/toantu.cpp
+++ PLGP2702/c++/toantu.cpp
@@ -57,12 +57,25 @@ int main(){
     */
     //---------------------------------toan tu so sanh
     // toan_hang1 toantusosanh toan_hang2 => true, false
+    /*
     int a = 100, b = 200;
     bool ok = a != b;
     cout << ok << endl;
+    */
+    int a = 100, b = 200, c = 300, d = 400, e = 200;
+    int res = ( a <= b ) && ( c <= d );
+    int res1 = ( a >= b ) && ( c <= d );
+    int res2 = (a == b ) || ( c > d );
+    int res3 = ( a < b ) || ( c <= d ) || ( b > c ) || ( a > e );
+    int res4 = !( a < b );
+    int res5 = !( a == e );  
+    cout << res << " "<< res1 << " " << res2 << " " << res3 << " " << res4 << " " << res5 << endl;
     return 0;
 }
 // assignment operator: toan tu gan 
 // toan tu toan hoc: +, -, *, /, %, ++, --
 //toan tu so sanh: >, <, >=, <=, ==, !=
 //logical: AND , OR , NOT
+// and : &&
+// or : ||
+// not : !
