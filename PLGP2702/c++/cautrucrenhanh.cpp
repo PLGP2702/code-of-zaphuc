b1bd2c202793d74bde04f00ca484c51e1c5568b4
PLGP2702 <phucphan2702@gmail.com>
2022-09-28 21:23:26 +0700

    0930920922

--- PLGP2702/c++/cautrucrenhanh.cpp
+++ PLGP2702/c++/cautrucrenhanh.cpp
@@ -27,9 +27,10 @@ int main(){
         cout << "la so le !\n";
     }
 */
+/*
     int n;
     cin >> n;
-    if(n ==1){
+    if(n == 1){
         cout << "chu nhat\n";
     }
     else if(n == 2){
@@ -53,6 +54,67 @@ int main(){
     else{
         cout << "du lieu khong hop le!";
     }
+*/
+/* 
+    int n; 
+    cin >> n;
+    if((n == 1) || (n == 3) || (n == 5) || (n == 7) || (n == 8) || (n == 10) || (n == 12)){
+        cout << "Thang co 31ngay\n";
+    }
+    else if((n == 4) || (n == 6) || (n == 9 ) || (n == 11)){
+        cout << "Thang co 30 ngay\n";
+    }
+    else if(n == 2){
+        cout << "Thang co 28 ngay\n";
+    }
+    else{
+        cout << "du lieu khong hop le!!";
+    }
+*/
+/*
+    int n;
+    cin >> n;
+    switch (n) {
+        case 1:
+            cout << "Monday";
+            break;
+        case 2:
+            cout << "Tuesday";
+            break;
+        case 3:
+            cout << "Wednesday";
+            break;
+        case 4:
+            cout << "Thursday";
+            break;
+        case 5:
+            cout << "Friday";
+            break;
+        case 6:
+            cout << "Saturday";
+            break;
+        case 7:
+            cout << "Sunday";
+            break;
+            default:
+            cout <<"du lieu khong hop le!!!";
+        }
+*/
+    int n; cin >> n;
+    switch (n)
+    {
+    case 1 : case 3 : case 5 : case 7 : case 8 : case 10 : case 12:
+        printf("31 ngay\n");
+        break;
+    case 4 : case 6 : case 9 : case 11:
+        printf("30 ngay\n");
+        break;
+    case 2:
+        printf("28 ngay\n");
+        break;
+    default:
+        printf("du lieu khong hop le!");
+    }        
     return 0;  
 }
 
