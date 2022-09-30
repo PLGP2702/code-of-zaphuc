c8be3b294bf54c75ebaa814e341cb978688c4f48
PLGP2702 <phucphan2702@gmail.com>
2022-09-30 19:55:11 +0700

    rfghfdgh

--- PLGP2702/c++/baitaprenhanh.cpp
+++ PLGP2702/c++/baitaprenhanh.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main(){
     int a, b;
     cin >> a >> b;
-    
+    int tong = a + b;
+    int hieu = a - b;
+    int tich = a * b;
+    int thuong = (float)a / b;
+    cout << tong << " " << hieu << " " << tich << " " << thuong << endl;
     return 0;
 }
