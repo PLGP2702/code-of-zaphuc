d1be56a837a4c3cf3f436951d08f43fd286d8358
PLGP2702 <phucphan2702@gmail.com>
2022-09-30 21:01:05 +0700

    phuc10

--- PLGP2702/c++/helloworld.cpp
+++ PLGP2702/c++/helloworld.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
 #include <iomanip>
 
-// using namespace std;
-
 // int main() {
 //     cout << "Hello world!" << endl;
 //     return 0;
 // }
 using namespace std;
 int main(){ 
-    /*cout << sizeof(bool) << endl;
+    cout << sizeof(bool) << endl;
     cout << INT_MIN << " " << INT_MAX << endl;
-    cout << LLONG_MIN << " " << LLONG_MAX << endl;
-    */
-    /*
+    cout << LLONG_MIN << " " << LLONG_MAX << endl; 
+    /* //in ra man hinh
     int bankinh;
     cout << bankinh << endl;
     cout << "xin chao" << endl;
     */
-    /*
+    /* //kieu long long
     long long a, b, c;
     cin >> a >> b >> c;
     cout << "gia tri la: " << a << endl;
     cout << "gia tri la: " << b << endl;
     cout << "gia tri la: " << c << endl;
     */
-    /*
+    /* //kieu float
     float a; 
     cin >> a;
     cout << fixed << setprecision(10) << a << endl;
     */
-    /*
+    /* //kitu
     char kitu;
     cin >> kitu;
     cout << kitu << endl;
     */
-    /*
+    /* //dung sai
     bool ok = true, oke = false;
     cout << ok << endl;
     cout << oke << endl;
     */
+    /*
     int a;
     cout << "Nhap gia tri cho a: ";
     cin >> a;
     cout << "Gia tri cua a la: " << a << endl;
+    */
     return 0;
 }
 
