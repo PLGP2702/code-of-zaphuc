ae475e9098dfdc8a126457148f85b3824a5f5d69
PLGP2702 <phucphan2702@gmail.com>
2022-10-04 16:04:41 +0700

    phuc31

--- PLGP2702/python/N16/chuong4/cau4.py
+++ PLGP2702/python/N16/chuong4/cau4.py
@@ -1,19 +1,49 @@
 #bai4
-'''cau a'''
+#CAU A
 def uocso(n):
     ans = 0
     for i in range(1, n):
         if n % i == 0:
             ans += 1
     return ans
-print(uocso(10))
-'''cau b'''
+n = int(input("Nhap n:"))
+print("Cau A:",uocso(20))
+#CAU B
 def snt(n):
     dem = 0
     for i in range (1,n):
         if  n % i == 0: dem = dem + 1
     if dem != 1: return 0
     else : return 1 
-n = int(input())
-print (snt(n))
-    
+print("Cau B:", snt(n))
+# #CAU C
+def uocsole(n):
+    cnt = 0
+    for i in range(1, n+1):
+        if n % i == 0 and i % 2 == 1:
+            cnt += 1
+    return cnt
+print("Cau C:", uocsole(n))
+#CAU D
+def uocsonguyento(n):
+    def snt(k):
+        if k < 2:
+            return 0
+        for i in range(2, k):
+            if k % i == 0:
+                return 0
+        return 1
+    dem = 0
+    for j in range (1,n+1):
+        if n % j == 0 and snt(j) == 1:
+            dem += 1
+    return dem
+print("Cau D:",uocsonguyento(n))
+#CAU E
+def uocsothucsu(n):
+    ans = 0
+    for i in range(1, n):
+        if n % i == 0:
+            ans += i
+    return ans
+print("Cau E:",uocsothucsu(n))
