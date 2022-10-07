5f340df388903b4a4bae21ea82100ae4a1d4e463
PLGP2702 <phucphan2702@gmail.com>
2022-10-07 21:13:30 +0700

    phuc38

--- PLGP2702/c++/baitaprenhanh.cpp
+++ PLGP2702/c++/baitaprenhanh.cpp
@@ -177,7 +177,7 @@ int main(){
     }
     cout << c << endl;
     */
-     //chữ cái kế tiếp--------------------------------------------
+    /* //chữ cái kế tiếp--------------------------------------------
     char c; 
     cin >> c;
     if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
@@ -195,7 +195,7 @@ int main(){
     else{
         cout << "INVALID\n";
     }
-    
+    */
     /* //số lớn số nhỏ----------------------------------------------                                                                                             
     int a, b, c;
     cin >> a >> b >> c;
@@ -223,5 +223,29 @@ int main(){
     if(c > max) max = c;
     cout << max <<" "<< min << endl;
     */
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a){
+        //kiem tra xem tam giac do la tam giac gi
+        if(a == b && a == c){
+            cout << 1 << endl;
+        }
+        else if(a == b || a == c || b == c){
+            if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b){
+                cout << 4 << endl;
+            }
+            else{
+                cout << 2 << endl;
+            }
+        }
+        else if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b){
+            cout << 3 << endl;
+        }
+        else{
+            cout << 5 << endl;
+        }
+    }
+    else
+        cout <<"INVALID";
     return 0;
 }
