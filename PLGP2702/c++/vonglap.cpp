9df5cdda0eea3da0c7edefca24a650333c3e28b1
PLGP2702 <phucphan2702@gmail.com>
2022-10-10 19:50:59 +0700

    phuc47

--- PLGP2702/c++/vonglap.cpp
+++ PLGP2702/c++/vonglap.cpp
@@ -14,11 +14,19 @@ int main()
     //     if(i == 40)
     //         break;
     // }
-    int n = 100;
-    for(int i = 1; i <= 100; i++){
-        cout <<"HI !\n";
-        continue;
-        cout <<"Zaphuc2702\n";
+    // int n = 100;
+    // for(int i = 1; i <= 100; i++){
+    //     cout <<"HI !\n";
+    //     continue;
+    //     cout <<"Zaphuc2702\n";
+    // }
+    int n;
+    cout << "Nhap gia tri cua n: ";
+    cin >> n;
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        S += i;
     }
+    cout << S << endl;
     return 0;
 }
