a0ac957aaf0ef9f855f0825db20bb709c82f7282
PLGP2702 <phucphan2702@gmail.com>
2022-10-10 20:04:48 +0700

    phuc50

--- PLGP2702/c++/vonglap.cpp
+++ PLGP2702/c++/vonglap.cpp
@@ -29,7 +29,22 @@ int main()
     }
     cout << s << endl;
     */
-     //nhập vào giá trị nguyên dương ,tính tổng sau và in ra kết quả màn hình.
-    
+    /* //nhập vào giá trị nguyên dương ,tính tổng sau và in ra kết quả màn hình.
+    int n;
+    cout << "Nhap gia tri cua n: "; cin >> n;
+    int s = 0;
+    for(int i = 1; i <= n; i++){
+        s += i * i;
+    }
+    cout << s << endl;
+    */
+    int n;
+    cout << "Nhap gia tri cua n: "; cin >> n;
+    int s = 0;
+    for(int i = 1; i <= n; i++){
+        if(i % 3 == 0)
+            s += i;
+    }
+    cout << s << endl;
     return 0;
 }
