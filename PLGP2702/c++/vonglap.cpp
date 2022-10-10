85e538ea8b37f7cda43c31bda45ad83e58eb235d
PLGP2702 <phucphan2702@gmail.com>
2022-10-10 19:36:47 +0700

    phuc44

--- PLGP2702/c++/vonglap.cpp
+++ PLGP2702/c++/vonglap.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
 using namespace std;
-
+//break
+//continue
 int main()
 {
     // for (int i = 1; i <= 5; i++){
     //     cout << "Hello !\n";
     // }
+    // int n = 100;
+    // for(int i = n; i >= 1; i--){
+    //     cout << i << endl;
+    //     if(i == 40)
+    //         break;
+    // }
     int n = 100;
-    for(int i = n; i >= 1; i--){
-        cout << i << endl;
+    for(int i = n; i <= 10; i++){
+        cout <<"HI !\n";
+        // continue;
+        cout <<"Zaphuc\n";
     }
     return 0;
 }
