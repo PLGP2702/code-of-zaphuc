aac169be2f61c6ee537d59e948c5ed82f9d2fe69
PLGP2702 <phucphan2702@gmail.com>
2022-10-10 17:00:06 +0700

    phuc42

--- PLGP2702/c++/baitaprenhanh.cpp
+++ PLGP2702/c++/baitaprenhanh.cpp
@@ -280,22 +280,26 @@ int main(){
         if(b == 0 && c == 0){
             cout <<"INF";
         }
+        else if(b == 0 && c != 0){
+            cout <<"No";
+        }
         else if(b != 0){
-            cout << fixed << Setprecision(2) << (float)- c / b;
+            cout << fixed << setprecision(2) << (float)- c / b;
         }
     }
     else{
         int delta = b * b - 4 * a * c;
-        if(delta < 0)
+        if(delta < 0){
             cout <<"No\n";
-    }
-    else if(delta == 0){
+        }
+        else if(delta == 0){
         cout << fixed << setprecision(2) << (float)-b/(2 * a);
-    }
-    else{
-        float x1 = (-b + sqrt(delta)) / (2 * a);
-        float x2 = (-b - sqrt(delta)) / (2 * a);
-        cout << fixde << setprecision(2) << x1 << " " << x2 << endl;
+        }
+        else{
+            float x1 = (-b + sqrt(delta)) / (2 * a);
+            float x2 = (-b - sqrt(delta)) / (2 * a);
+            cout << fixed << setprecision(2) << x1 << " " << x2 << endl;
+        }    
     }
     return 0;
 }
