2be8568c4ee85b0a39b3523d5f374ac218e8fc5d
PLGP2702 <phucphan2702@gmail.com>
2022-10-26 21:04:49 +0700

    9:0010/26/2022

--- PLGP2702/c++/vonglap.cpp
+++ PLGP2702/c++/vonglap.cpp
@@ -32,31 +32,36 @@ int main()
     // }
     /* //nhập vào giá trị nguyên dương ,tính tổng sau và in ra kết quả màn hình. 
     int n;
-    cout << "Nhap gia tri cua n: "; cin >> n;
+    cout << "Nhap gia tri n: ";
+    cin >> n;
     int s = 0;
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++)
+    {
         s += i;
     }
-    cout << s << endl;
+    cout << "tong la: " << s << endl;
     */
     /* //nhập vào giá trị nguyên dương ,tính tổng sau và in ra kết quả màn hình.
     int n;
-    cout << "Nhap gia tri cua n: "; cin >> n;
+    cout << "Nhap gia tri n: ";
+    cin >> n;
     int s = 0;
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++)
+    {
         s += i * i;
     }
     cout << s << endl;
     */
-    /*
+    
     int n;
-    cout << "Nhap gia tri cua n: "; cin >> n;
+    cout << "Nhap gia tri cua n: "; 
+    cin >> n;
     int s = 0;
     for(int i = 1; i <= n; i++){
         if(i % 3 == 0)
             s += i;
     }
     cout << s << endl; 
-    */
+    
     return 0;
 }
