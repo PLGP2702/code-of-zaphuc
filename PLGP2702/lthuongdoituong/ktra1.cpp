2c5c8f99f9a4bf0170419861b17c4ae48e10541b
PLGP2702 <phucphan2702@gmail.com>
2022-12-01 14:04:02 +0700

    1/12/2022

--- PLGP2702/lthuongdoituong/ktra1.cpp
+++ PLGP2702/lthuongdoituong/ktra1.cpp
@@ -3,7 +3,8 @@
     
 using namespace std;
      
-class Complex{
+class Complex
+{
     private:
         float thuc, ao;
     public:
@@ -17,7 +18,7 @@ class Complex{
         thuc = a;
         ao = b;
     }
-    
+
         void nhap() {
             cout << "Nhap phan thuc: "; cin >> thuc;
             cout << "Nhap phan ao: "; cin >> ao;    
@@ -66,22 +67,20 @@ class Complex{
         // void chia(Sophuc a, Sophuc b) {
         //     thuc = (a.thuc * b.thuc + a.ao * b.ao) / (b.ao * b.ao + b.thuc * b.thuc);
         //     ao = (a.ao * b.thuc - a.thuc * b.ao) / (b.thuc * b.thuc + b.ao * b.ao);
-        // };
-    };
-    
-    int main()
-    {
-        Complex a,b,c;
-        a.nhap();
-        b.nhap();
-        c.cong(a,b);
-        cout << "Tong cua 2 so phuc: "; c.xuat();
-        c.tru(a,b);
-        cout << "\nHieu cua 2 so phuc: "; c.xuat();
-        c.nhan(a,b);
-        cout << "\nTich cua 2 so phuc: "; c.xuat();
-        // c.chia(a,b);
-        // cout << "\nThuong cua 2 so phuc: "; c.xuat();
-        return 0;
-    }
+        // };   
+
+{
+    Complex a,b,c;
+    a.nhap();
+    b.nhap();
+    c.cong(a,b);
+    cout << "Tong cua 2 so phuc: "; c.xuat();
+    c.tru(a,b);
+    cout << "\nHieu cua 2 so phuc: "; c.xuat();
+    c.nhan(a,b);
+    cout << "\nTich cua 2 so phuc: "; c.xuat();
+    // c.chia(a,b);
+    // cout << "\nThuong cua 2 so phuc: "; c.xuat();
+    return 0;
+}
     
---
+++ PLGP2702/lthuongdoituong/test.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Complex
+{
+    private:
+        float ao, thuc;
+    public:
+    //
+    Complex()
+    {
+        thuc = 0;
+        ao = 0;
+    }
+    //
+    Complex(float a, float b)
+    {
+        thuc = a;
+        ao = b;
+    }
+    //
+    void nhap()
+    {
+        cout << "\nNhap phan thuc: "; cin >> thuc;
+        cout << "\nNhap phan ao: "; cin >> ao;
+    };
+    //
+    void xuat()
+    {
+        if(ao > 0)
+            cout << thuc << "+i" << ao;
+        else if(ao == 1)
+            cout << thuc << "+i";
+        else if(ao == -1)
+            cout << thuc << "-i";
+        else if(ao == 0)
+        {
+            cout << thuc;
+        }
+        else
+            cout << thuc << ao << "i";
+    };
+    //
+    void setthuc(float a)
+    {
+        thuc = a;
+    }
+    //
+    float getthuc()
+    {
+        return thuc;
+    }
+    //
+    void setao(float b)
+    {
+        ao = b;
+    }
+    //
+    float getao(float b)
+    {
+        return ao;
+    }
+    //
+    void Cong(Complex a, Complex b)
+    {
+        thuc = a.thuc + b.thuc;
+        ao = a.ao + b.ao;
+    };
+    //
+    void Tru(Complex a, Complex b)
+    {
+        thuc = a.thuc - b.thuc;
+        ao = a.ao - b.ao;
+    };
+    //
+    void Nhan(Complex a, Complex b)
+    {
+        thuc = a.thuc * b.thuc - a.ao * b.ao;
+        ao = a.thuc * b.ao + a.ao * b.thuc;
+    };
+    //
+    void Chia(Complex a, Complex b)
+    {
+        thuc = (a.thuc * b.thuc + a.ao * b.ao) / (a.ao * b.ao + a.thuc * b.thuc);
+        ao = (a.ao * b.thuc - a.thuc * b.ao) / (b.thuc * b.thuc - b.ao * b.ao);
+    };
+};
+
+int main()
+{
+    Complex a, b, c;
+    a.nhap();
+    b.nhap();
+    c.Cong(a, b);
+    cout << "\nTong cua hai so thuc: "; c.xuat();
+    c.Tru(a, b);
+    cout << "\nHieu cua hai so thuc: "; c.xuat();
+    c.Nhan(a, b);
+    cout << "\nTich cua hai so thuc: "; c.xuat();
+    c.Chia(a, b);
+    cout << "\nThuong cua hai so thuc: "; c.xuat();
+    return 0;
+};
