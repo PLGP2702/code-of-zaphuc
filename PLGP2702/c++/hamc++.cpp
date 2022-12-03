5076d4887f5b24275cd7bf5eb086c46b1783a154
PLGP2702 <phucphan2702@gmail.com>
2022-12-03 14:49:44 +0700

    phcu28943732

--- PLGP2702/c++/hamc++.cpp
+++ PLGP2702/c++/hamc++.cpp
@@ -1,3 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void Xinchao()
+{
+    cout << "hello Phuc\n";
+}
+
+void Thamso(int a, int b , float c)
+{
+    cout << a << " " << b << " " << c << endl;
+    cout << "\nTong: " << a + b + c;
+    cout << "\nHieu: "<< c - b - a;
+}
+
+void Tang(int n)
+{
+    n += 100;
+}
+
+//S = 1 + 2 + 3 + 4 + ..... +n 
+int Tong(int n)
+{
+    return n * (n + 1) / 2;
+}
+
+int main()
+{
+    // int n;
+    // cout << "Nhap vao so lan muon in ra: ";
+    // cin >> n;
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     Xinchao();
+    // }
+
+    //Thamso(100, 200, 300); //goi ham
+
+    //float n = 10, m = 20, l = 30.5;
+    //Thamso(n, m, l);
+    
+    //int a = 1000;
+    //Tang(a);
+    //cout << a << endl;
+
+    int a = 10;
+    cout << Tong(a) << endl;
+    return 0;
+}
